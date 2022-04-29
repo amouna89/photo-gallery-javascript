a5cd1a6123f51a7761dbf6bf6185796095f183ff
amouna89 <95442375+amouna89@users.noreply.github.com>
2022-04-29 13:15:21 -0500

    simplifiy the javascript code

--- js/app.js
+++ js/app.js
@@ -6,86 +6,32 @@ baguetteBox.run('.gallery');
 var input = document.getElementById("search");
 
 
-
-document.addEventListener('keyup', logKey);//CALL the function logkey
-
 //define the function logkey due to hoisting :javascript engine loqds the function declaration first then calls the function 
 function logKey(e) {
   var input = document.getElementById("search").value;
     var inputLower = input.toLowerCase();
      const nodeList = document.querySelectorAll("a");
-     const imgList =document.querySelectorAll("img");
+  
 
 for (let i = 0; i < nodeList.length; i++) {//begin for
   //retourner le data-caption de chaque anchor <a>
   let data = nodeList[i].getAttribute("data-caption");
-  var img = imgList[i];
+
   var dataLower = data.toLowerCase();  
   var result = dataLower.includes(inputLower);
 
-  if (!result){
-   //object.style.display = "none" pour cacher l'objet
-   console.log("valeur non trouvé",img);
-   var altimg=img.getAttribute("alt");
-   console.log(altimg);
-   if (altimg != "icon"){//pour ne pas cacher l'icon car c'est une image aussi
-   img.style.display="none";
-   }//endif altimg
-
-  }//end if result
-
+  if(result){
+    nodeList[i].style.display="";
+  }
+  else{
+    nodeList[i].style.display="none";
+  }
   
-  
- 
 
 }//end for
 
-  }//end logkey function
+}//end logkey function
   
 
 
-
-
-// input.addEventListener("keypress", function(event) {
-//   if (event.key === "Enter") {//begin if
-// //variable locale elle n est pas visile en dehors de la fonction 
-
-//     var input = document.getElementById("search").value;
-//     var inputLower = input.toLowerCase();
-//      const nodeList = document.querySelectorAll("a");
-//      const imgList =document.querySelectorAll("img");
-
-// for (let i = 0; i < nodeList.length; i++) {//begin for
-//   //retourner le data-caption de chaque anchor <a>
-//   let data = nodeList[i].getAttribute("data-caption");
-//   var img = imgList[i];
-//   var dataLower = data.toLowerCase();  
-//   var result = dataLower.includes(inputLower);
-
-//   if (result){
-//     // console.log("valeur trouve");
-//     // console.log(data);
-
-//   }
-
-//   else {//object.style.display = "none" pour cacher l'objet
-//     console.log("valeur non trouvé",img);
-//     var altimg=img.getAttribute("alt");
-//     console.log(altimg);
-//     if (altimg!="icon"){//pour ne pas cacher l'icon car c'est une image aussi
-//     img.style.display="none";
-//     }//endif altimg
-
-//   }//end else
-  
- 
-
-// }//end for
-
-//   }//end if
-// });
-
-
- 
-
-
+  document.addEventListener('keyup', logKey);//CALL the function logkey
